03a54cf95253aa2a90a88473b6cd375e58a539ca
Bomin Zhang <zhangbomin@megaease.com>
2021-08-13 17:47:05 +0800

    update example

--- examples/flashsale/index.ts
+++ examples/flashsale/index.ts
@@ -18,32 +18,26 @@
 // This exports all functions required by Easegress
 export * from '../../easegress/proxy'
 
-import { Program, response, parseDate, cluster, rand, getUnixTimeInMs, registerProgramFactory, log, LogLevel } from '../../easegress'
+import { Program, response, parseDate, cluster, rand, getUnixTimeInMs, registerProgramFactory, request } from '../../easegress'
 
 class FlashSale extends Program {
-	// permitted is the number of requests which are permitted to enter the
-	// flash sale product page
-	permitted: i32
-
-	// maxPermission is the upper limits of permitted requests, 
-	maxPermission: i32
+	// startTime is the start time of the flash sale, unix timestamp in millisecond
+	startTime: i64
 
 	// blockRatio is the ratio of requests being blocked to protect backend service
 	// for example: 0.4 means we blocks 40% of the requests randomly.
 	blockRatio: f64
 
-	// startTime is the start time of the flash sale
-	startTime: i64
+	// maxPermission is the upper limits of permitted users 
+	maxPermission: i32
 
 	constructor(params: Map<string, string>) {
 		super(params)
 
-		this.permitted = 0
-
-		let key = "maxPermission"
+		let key = "startTime"
 		if (params.has(key)) {
 			let val = params.get(key)
-			this.maxPermission = i32(parseInt(val))
+			this.startTime = parseDate(val).getTime()
 		}
 
 		key = "blockRatio"
@@ -52,18 +46,14 @@ class FlashSale extends Program {
 			this.blockRatio = parseFloat(val)
 		}
 
-		key = "startTime"
+		key = "maxPermission"
 		if (params.has(key)) {
 			let val = params.get(key)
-			this.startTime = parseDate(val).getTime()
+			this.maxPermission = i32(parseInt(val))
 		}
 	}
 
 	run(): i32 {
-		super.run()
-		let counter = cluster.addInt32("counter", 3)
-		log(LogLevel.Warning, "counter is " + counter.toString())
-
 		// if flash sale not start yet
 		if (getUnixTimeInMs() < this.startTime) {
 			// we just set response body to 'not start yet' here, in practice,
@@ -73,10 +63,19 @@ class FlashSale extends Program {
 			return 1
 		}
 
-		if (this.permitted < this.maxPermission && rand() > this.blockRatio) {
-			// the lucky guy
-			this.permitted++
+		// check if the user was already permitted
+		let id = request.getHeader("Authorization")
+		if (cluster.getString("id/" + id) == "true") {
 			return 0
+		}
+
+		// check if we have reach the upper limits
+		if (cluster.countKey("id/") < this.maxPermission) {
+			if (rand() > this.blockRatio) {
+				// add the lucky guy to permitted users
+				cluster.putString("id/" + id, "true")
+				return 0
+			}
 		}
 
 		// block this request, set response body to `sold out`
